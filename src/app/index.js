446cfddc90fdce1e6c921c0622888de008322444
Moh Rizal Putra Hadinata <rizalputraha@gmail.com>
2018-12-05 14:13:45 +0700

    New Update Service Worker Plugin Webpack

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
+OfflinePluginRuntime.install();
 ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,41 @@ const path = require('path');
  */
 
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OfflinePlugin = require('offline-plugin');
 
 const DIST_DIR = path.resolve(__dirname, "dist");
 const SRC_DIR = path.resolve(__dirname, "src");
 
 module.exports = {
+    devtool: 'source-map',
     entry: SRC_DIR + '/app/index.js',
     output: {
         path: DIST_DIR,
-        filename: '[name].bundle.js',
+        filename: '[name].[chunkhash].js',
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        },
     },
     module: {
         rules: [
@@ -46,5 +72,22 @@ module.exports = {
         compress: true,
         port: 8080
     },
-    plugins: [new UglifyJSPlugin()]
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new UglifyJSPlugin({
+            include: /dist/,
+        }),
+        new HtmlWebpackPlugin({
+            template: DIST_DIR + '/index.html',
+            filename: 'index.html'
+        }),
+        new OfflinePlugin({
+            ServiceWorker: {
+                minify: true,
+            }
+        })
+    ],
+
 };
