d42aaf8ca631728e79404927ba241b8566cf5c96
Moh Rizal Putra Hadinata <rizalputraha@gmail.com>
2019-01-17 16:05:13 +0700

    Updates

---
+++ precache-manifest.6a85a0371888c9b5001de56f59e6c71c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4ba655d9ad4cc6b87c5",
+    "url": "/static/js/main.c4ba655d.chunk.js"
+  },
+  {
+    "revision": "5fd03fcf31dd4041dbb5",
+    "url": "/static/js/1.5fd03fcf.chunk.js"
+  },
+  {
+    "revision": "c4ba655d9ad4cc6b87c5",
+    "url": "/static/css/main.dcab6bc9.chunk.css"
+  },
+  {
+    "revision": "5fd03fcf31dd4041dbb5",
+    "url": "/static/css/1.11576ca0.chunk.css"
+  },
+  {
+    "revision": "4a56152a02cca55134b0cb1f60667b7c",
+    "url": "/index.html"
+  }
+];
